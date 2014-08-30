231f894ac9aa6f86bbb591231bb37cf73ceec551
Tarso de Castro <tarsodecastro@gmail.com>
2014-08-29 21:50:10 -0300

    coluna

---
+++ js/coluna.js
@@ -0,0 +1,47 @@
+ $(document).ready(function(){
+ 		
+	 var oTable = $('#tabela').dataTable( {
+         "bJQueryUI": true,
+         "bStateSave": false,
+         "bPaginate": true,
+         "sPaginationType": "full_numbers",
+         "bSort": false,
+         "bFilter": true,
+         "bLengthChange": false,
+         "bInfo": true,
+         "aoColumns": [
+       				{"sWidth":"30px", "sClass": "text-center"},
+       				{"sClass": "text-center"},					
+       				{"sClass": "text-center"} 								 	
+       			],
+     });
+	
+	$("#tabela tr").mouseover(function(){
+		$(this).addClass("tableRow_mouseover");
+	});
+	
+	$("#tabela tr").mouseout(function(){
+		$(this).removeClass("tableRow_mouseover");
+	});  
+  	 
+ 	
+	$('#busca').click(function() { 
+        $.blockUI({ message: $('#buscaForm') }); 
+ 
+        //setTimeout($.unblockUI, 2000); 
+    }); 
+
+    $("#search").focus(function() {
+        if($("#search").val() == 'pesquisa textual'){
+            $("#search").attr('value','');
+            $("#search").removeClass('search_text');
+        }
+    }).blur(function() {
+        if($("#search").val() == ''){
+            $("#search").attr('value','pesquisa textual');
+            $("#search").addClass('search_text');
+        }
+    });
+
+			
+ });	
