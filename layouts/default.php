18ea3fd3fea85d64dda680ba6adbb9ea76fb2228
Tarso de Castro <tarsodecastro@gmail.com>
2014-10-24 17:30:07 -0300

    rodapé

--- layouts/default.php
+++ layouts/default.php
@@ -341,10 +341,11 @@ $SessTimeLeft = ($SessExpTime - ($CurrTime - $arrLastActivity[0]["last_activity"
 	                    	<!-- 
 	                        <li><a href="#" class="icoRss" title="Rss"><i class="fa fa-rss"></i></a></li>
 	                        <li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
-	                         -->
+	                         
 	                        <li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
 	                        <li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>
 	                        <li><a href="#" class="icoGoogle" title="Google +"><i class="fa fa-google-plus"></i></a></li>
+	                        -->
 	                        
 	                    </ul>				
 					</div>
